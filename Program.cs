8ebdc977bd5763b8951b885b912e4c0aac092c53
alexanderhoyskel <1664AlfredAnna>
2025-09-11 10:15:29 +0200

    Update db

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MyShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ItemDbContext>(options => {
+    options.UseSqlite(
+        builder.Configuration["ConnectionStrings:ItemDbContextConnection"]);
+});
 
 var app = builder.Build();
 
